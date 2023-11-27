8c6e1068384d53368d3d6be0cbae7fcdbe3ea7a1
Igor <igor.torquato.20@gmail.com>
2023-11-27 17:26:37 -0300

    att faixa login

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,28 +1,40 @@
-import React from 'react'
-import './Login.css'
-import { FormLogin } from '../../components/Forms/FormLogin'
+import React from "react";
+import "./Login.css";
+import { FormLogin } from "../../components/Forms/FormLogin";
+import { useNavigate } from "react-router-dom";
 
 export const Login = () => {
+  const navigate = useNavigate();
+
+  const navigateToPage = (path) => {
+    navigate(path);
+  };
+
   return (
     <>
-    <div className="login-container">
+      <div className="login-container">
         <div className="login-background-image"></div>
         <div className="login-formulario">
           <div className="login-logo">
-            <span className='faixa'>
-            d.<span className="text-ia">IA</span>gnóstica
+            <span
+              className="login-faixa"
+              onClick={() => {
+                navigateToPage("/");
+              }}
+            >
+              d.<span className="text-ia">IA</span>gnóstica
             </span>
           </div>
           <div className="login-section">
             <div className="login-welcome">
-            <h2>Seja Bem-Vindo</h2>
+              <h2>Seja Bem-Vindo</h2>
             </div>
             <div className="login-form">
-              <FormLogin/>
+              <FormLogin />
             </div>
           </div>
-        </div>    
-    </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
