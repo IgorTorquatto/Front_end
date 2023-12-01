101191d2632043a7018bec0f62a89eef656e0250
Igor <igor.torquato.20@gmail.com>
2023-12-01 18:30:09 -0300

    fix: login console

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,41 +1,55 @@
-import React from 'react'
-import './Footer.css'
-import { DiagnosticaLogo } from '../Logo/DiagnosticaLogo'
-import { FaEnvelope, FaGithub} from 'react-icons/fa6'
+import React from "react";
+import "./Footer.css";
+import { DiagnosticaLogo } from "../Logo/DiagnosticaLogo";
+import { FaEnvelope, FaGithub } from "react-icons/fa6";
 
 export const MyFooter = () => {
-
   return (
-    <div class="container-fluid footer_container pt-5">
-      <div class="row justify-content-around mb-5">
-        <div class="col-md-auto d-flex my-3 f-logo_context">
+    <div className="container-fluid footer_container pt-5">
+      <div className="row justify-content-around mb-5">
+        <div className="col-md-auto d-flex my-3 f-logo_context">
           <DiagnosticaLogo />
-        </div>  
+        </div>
 
-        <div class="col-md-auto my-3">
-          <h5 style={{color: '#F8F8FF'}}>Informações</h5>
-          <div class="footer_item">
-            <FaEnvelope className='f_icon' />
-            <span className='footer_text'>residenciaticgia@gmail.com</span>
+        <div className="col-md-auto my-3">
+          <h5 style={{ color: "#F8F8FF" }}>Informações</h5>
+          <div className="footer_item">
+            <FaEnvelope className="f_icon" />
+            <span className="footer_text">residenciaticgia@gmail.com</span>
           </div>
-          <a class="footer_item" href='https://github.com/grupodiagnostica' target='_blank'>
-            <FaGithub className='f_icon' />
-            <span className='footer_text'>GitHub/Diagnóstica</span>
+          <a
+            className="footer_item"
+            href="https://github.com/grupodiagnostica"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaGithub className="f_icon" />
+            <span className="footer_text">GitHub/Diagnóstica</span>
           </a>
         </div>
 
-        <div class="col-md-auto mr-10 my-3">
-          <h5 style={{color: '#F8F8FF'}}>Grupo Diagnóstica</h5>
-          <a className='f_location' href='https://maps.app.goo.gl/ocJoRj6kGdzqgTj59' target='_blank'>
-            <span className='footer_text'>Av. Tenente Raimundo Rocha Nº 1639,</span>
-            <span className='footer_text'>Bairro Cidade Universitária,</span>
-            <span className='footer_text'>Juazeiro do Norte - Ceará</span>
+        <div className="col-md-auto mr-10 my-3">
+          <h5 style={{ color: "#F8F8FF" }}>Grupo Diagnóstica</h5>
+          <a
+            className="f_location"
+            href="https://maps.app.goo.gl/ocJoRj6kGdzqgTj59"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <span className="footer_text">
+              Av. Tenente Raimundo Rocha Nº 1639,
+            </span>
+            <span className="footer_text">Bairro Cidade Universitária,</span>
+            <span className="footer_text">Juazeiro do Norte - Ceará</span>
           </a>
         </div>
       </div>
-      <div class="col-md text-center" style={{color: '#f8f8ffb0', fontSize:'smaller'}}>
+      <div
+        className="col-md text-center"
+        style={{ color: "#f8f8ffb0", fontSize: "smaller" }}
+      >
         Copyright © 2023
       </div>
     </div>
-  )
-}
+  );
+};
--- src/components/Forms/FormLogin.js
+++ src/components/Forms/FormLogin.js
@@ -1,45 +1,57 @@
-import React from 'react'
-import { Link, useNavigate } from 'react-router-dom';
-import './FormLogin.css'
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./FormLogin.css";
 
-import { useEffect, useState } from 'react';
-import { AiOutlineEye, AiOutlineEyeInvisible, AiOutlineInfoCircle } from 'react-icons/ai';
-import * as yup from 'yup'
-import { useForm, SubmitHandler } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import { loadSession } from '../../store/ducks/tokens/actions.ts';
-import { api } from  '../../services/api.ts'
-import { useDispatch } from 'react-redux';
+//import { useEffect, useState } from "react";
+import {
+ // AiOutlineEye,
+ // AiOutlineEyeInvisible,
+  AiOutlineInfoCircle,
+} from "react-icons/ai";
+import * as yup from "yup";
+import { useForm/*, SubmitHandler*/ } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { loadSession } from "../../store/ducks/tokens/actions.ts";
+//import { api } from "../../services/api.ts";
+import { useDispatch } from "react-redux";
 
-const schema = yup.object({
-  email: yup.string().email('Informe um email valido').required('Informe um email valido'),
-  senha: yup.string().min(8, 'a senha deve conter 8 caracteres').required('Digite uma senha'),
-}).required();
+const schema = yup
+  .object({
+    email: yup
+      .string()
+      .email("Informe um email valido")
+      .required("Informe um email valido"),
+    senha: yup
+      .string()
+      .min(8, "a senha deve conter 8 caracteres")
+      .required("Digite uma senha"),
+  })
+  .required();
 
 export const FormLogin = () => {
-  const { register, handleSubmit, formState: { errors }, } = useForm({
-    resolver: yupResolver(schema)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
   });
 
-  
-  const history = useNavigate()
+  const history = useNavigate();
   const dispactch = useDispatch();
 
-  const [showPassword, setShowPassword] = useState('password')
-  const [visible, setVisible] = useState(true)
+  //const [showPassword, setShowPassword] = useState("password");
+  //const [visible, setVisible] = useState(true);
   const onSubmit = async (user) => {
-    console.log(user)
-    try{
-      dispactch(loadSession(user))
-      history('/sobre')
-    }
-    catch{
-      
-    }
+    console.log(user);
+    try {
+      dispactch(loadSession(user));
+      history("/diagnostico");
+    } catch {}
   };
   return (
     <>
-    <form className="custom-formcomp" onSubmit={handleSubmit(onSubmit)}>
+      <form className="custom-formcomp" onSubmit={handleSubmit(onSubmit)}>
         <div className="form-group mt-2 ">
           <label htmlFor="exampleFormControl1">Endereço de email</label>
           <input
@@ -49,7 +61,11 @@ export const FormLogin = () => {
             placeholder="exemplo@email.com"
             {...register("email")}
           />
-          <div className={errors.email ? 'showerror errorDiv' : 'hideerror errorDiv'}>
+          <div
+            className={
+              errors.email ? "showerror errorDiv" : "hideerror errorDiv"
+            }
+          >
             <AiOutlineInfoCircle />
             <p>{errors.email?.message}</p>
           </div>
@@ -64,7 +80,11 @@ export const FormLogin = () => {
             placeholder="Informe sua senha"
             {...register("senha")}
           />
-          <div className={errors.email ? 'showerror errorDiv' : 'hideerror errorDiv'}>
+          <div
+            className={
+              errors.email ? "showerror errorDiv" : "hideerror errorDiv"
+            }
+          >
             <AiOutlineInfoCircle />
             <p>{errors.email?.message}</p>
           </div>
@@ -73,7 +93,11 @@ export const FormLogin = () => {
         <p className="login-forget-password mt-3">
           <Link to="/">Esqueceu sua senha?</Link>
         </p>
-        <input type="submit" className="inputbtn btn btn-primary custom-btn" value="Entrar" />
+        <input
+          type="submit"
+          className="inputbtn btn btn-primary custom-btn"
+          value="Entrar"
+        />
       </form>
 
       <div className="form-login-text">
@@ -82,5 +106,5 @@ export const FormLogin = () => {
         </p>
       </div>
     </>
-  )
-}
+  );
+};
