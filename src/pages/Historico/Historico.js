bc3467bcb75c86d53386f134f5285c9e1aec8756
Raphael Pinheiro dos Santos <70273577+raphael-ps@users.noreply.github.com>
2023-12-11 10:41:24 -0300

    feat(Histórico): Adiciona ferramenta de busca
    - style(HistóricoCard): Adiciona mais interatividade aos cards

--- src/pages/Historico/Historico.js
+++ src/pages/Historico/Historico.js
@@ -1,19 +1,23 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { useEffect, useState } from 'react';
 import { NavbarComp } from '../../components/Header/NavbarComp'
 import { Box, border } from '@chakra-ui/react'
 import { MyFooter } from '../../components/Footer/Footer'
 import { HistoricoCard } from '../../components/Cards/HistoricoCard'
 import { LuSearch } from "react-icons/lu";
-import Select from 'react-select';
-
+import { api } from '../../services/api.ts'
+import { useDispatch, useSelector } from 'react-redux';
+import { Input } from '@chakra-ui/react'
 import './Historico.css'
 
 
 export const Historico = () => {
 
-  const [searchedText, setSearchedText] = useState("");
+  const [diagnosticosArray, setDiagnosticosArray] = useState([]);
+  const [diagnosticosOnDisplay, setDiagnosticosOnDisplay] = useState([]);
+  const { data: user } = useSelector((state) => state.tokens);
 
-  const data = [{
+  const diagnosticosExemple = [{
     exameID: '213213',
     exameType: 'Pneumonia', 
     dateTime: Date.now(), 
@@ -24,17 +28,63 @@ export const Historico = () => {
     sexo: 'Masculino',
     telefone: '(88) 98888-8888',
     bloodType: 'AB+'
+  },
+  {
+  exameID: '243213',
+  exameType: 'Pneumonia', 
+  dateTime: Date.now(), 
+  nomePaciente: 'Maria Joaquina Soares', 
+  cpf: '927.614.432-12',
+  examePositivo: false,
+  nascimento: '11/11/1999',
+  sexo: 'Feminino',
+  telefone: '(88) 98888-8888',
+  bloodType: 'AB+'
   }]
 
-  const initExemple = () => {
-    for (let index = 0; index < 30; index++) {
-      data.push(data[0]);
+  function initExemple(){
+    for (var index = 0; diagnosticosExemple.length < 30; index++) {
+      diagnosticosExemple.push(diagnosticosExemple[index%2]);
     }
+    setDiagnosticosArray(diagnosticosExemple)
+    setDiagnosticosOnDisplay(diagnosticosExemple)
+    console.log('Tamanho: ', diagnosticosArray.length)
   }
+
+  async function loadDiagnosticos() {
+    await api.get(`/diagnostico?id_medico=${user.data.id}`).then(({ data }) => {
+      console.log(data)
+    }).catch(({ err }) => {
+      console.log(err)
+    })
+  }
+
+  const searchOnHistory = (search) => {
+
+    var diagnosticos = diagnosticosArray.filter(item => {
+      var nome = item.nomePaciente.toLowerCase(), cpf = item.cpf.toLowerCase(), exameID = item.exameID.toLowerCase();
+      var searched = search.target.value.toLowerCase()
+      return nome.includes(searched) || cpf.includes(searched) || exameID.includes(searched)
+    })
+    setDiagnosticosOnDisplay(diagnosticos)
+
+    console.log('Diagnosticos Array: ', diagnosticosArray.length)
+    console.log('OnDisplay: ', diagnosticosOnDisplay.length)
+  }
+
+  useEffect(() => {
+    console.log('useEffect is running');
+    const fetchData = async () => {
+      console.log('Fetching data...');
+      await initExemple();
+      await loadDiagnosticos();
+    };
   
+    fetchData();
+  }, []);
+
   return (
     <Box className='historico-container'>
-      {initExemple()}
       <Box>
         <NavbarComp showEntrarButton={true}/>
       </Box>
@@ -44,22 +94,17 @@ export const Historico = () => {
             <Box>
 
             </Box>
-            <Select
-                id='historico-searchbar'
-                value={searchedText}
-                onChange={setSearchedText}
-                placeholder="Digite para buscar..."
-                menuIsOpen={false}
-                components={{ DropdownIndicator: false }}
-              />
+            <Input placeholder='Busque Por: Nome, CPF ou Número do Exame'  mr='0.5rem' onChange={searchOnHistory} backgroundColor={'white'}/>
+              {/*
               <Box className='search-icon' cursor={'pointer'} onClick={() => window.alert("Botão de buscar clicado")}>
                 <LuSearch className='icone-lupa' />
                 <span className='search-text'>Buscar</span>
               </Box>  
+              */}
           </Box>
           <Box id='result-context'>
             {
-              data.map((user, index) => {
+              diagnosticosOnDisplay.map((user, index) => {
                 return(
                   <Box className='historico-line' key={index}>
                     <HistoricoCard data={user}/>
