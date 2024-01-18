6172f7e92d08b646de5b37307f2829b46c587950
Eduardo Tavares <eduardo_tvn@hotmail.com>
2024-01-18 00:28:53 -0300

    Criando página Gerenciar IA

--- src/components/Clinica/GerenciarIA.js
+++ src/components/Clinica/GerenciarIA.js
@@ -1,7 +1,38 @@
 import React from 'react'
+import "./GerenciarIA.css";
+import { useState } from 'react';
+import {
+  Text,
+  Select,
+  Box,
+} from '@chakra-ui/react'
 
 export const GerenciarIA = () => {
+  const [selectedModel, setSelectedModel] = useState(null);
+  const models = [
+    { value: '1', label: 'Pneumonia - Crianças de até 5 anos' },
+    { value: '2', label: 'Pneumonia, Covid, Tuberculose - mapa de calor' },
+  ]
+
   return (
-    <div>GerenciarIA</div>
+    <div className='gerenciarIA-container'>
+      <div className='gerenciarIA-top'>
+        <h2>Gerenciar IA</h2>
+      </div>
+      <div className='gerenciarIA-model-select'>
+        <Box w='30%' padding='2rem 0'>
+            <Box w='100%' >
+              <Select
+                value={selectedModel}
+                onChange={setSelectedModel}
+                options={models}
+                isSearchable
+                placeholder="Selecione um modelo"
+              />
+            </Box>
+        </Box>
+      </div>
+    </div>
+    
   )
 }
