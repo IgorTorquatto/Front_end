f1e1960505d1dd8faaed5a53640aea11b597037a
Eduardo Tavares <eduardo_tvn@hotmail.com>
2024-01-18 17:19:10 -0300

    Seguimento de criação da página

--- src/components/Clinica/GerenciarIA.js
+++ src/components/Clinica/GerenciarIA.js
@@ -2,9 +2,12 @@ import React from 'react'
 import "./GerenciarIA.css";
 import { useState } from 'react';
 import {
-  Text,
   Select,
   Box,
+  Container,
+  Grid,
+  Text,
+  Button,
 } from '@chakra-ui/react'
 
 export const GerenciarIA = () => {
@@ -28,9 +31,50 @@ export const GerenciarIA = () => {
                 options={models}
                 isSearchable
                 placeholder="Selecione um modelo"
+                background={'white'}
               />
             </Box>
         </Box>
+      </div>
+      <div className='gerenciarIA-model-details'>
+        <Container background={'blue.600'} maxW={'container.lg'} h={40} borderRadius={'20px'} ml={'5'}>
+          <Box display={'flex'} alignItems={'center'} paddingTop={'8'}>
+            <Grid templateColumns={{ base: 'repeat(2, 1fr)', md: 'repeat(2, 1fr)', lg: 'repeat(2, 1fr)' }} gap={4}>
+              <Container background={'white'} borderRadius={'10px'} mb={{ base: 2, md: 0 }} h={10} display={'flex'} alignItems={'center'}>
+                Precisão: "Valor placeholder"
+              </Container>
+              <Container background={'white'} borderRadius={'10px'} mb={{ base: 2, md: 0 }} h={10} display={'flex'} alignItems={'center'}>
+                Acurácia: "Valor placeholder"
+              </Container>
+              <Container background={'white'} borderRadius={'10px'} mb={{ base: 2, md: 0 }} h={10} display={'flex'} alignItems={'center'}>
+                F1-Score: "Valor placeholder"
+              </Container>
+              <Container background={'white'} borderRadius={'10px'} mb={{ base: 2, md: 0 }} h={10} display={'flex'} alignItems={'center'}>
+                Recall: "Valor placeholder"
+              </Container>
+            </Grid>
+          </Box>
+        </Container>
+      </div>
+      <div className='gerenciarIA-image-bank'>
+        <h4>Existem X novas imagens no banco de imagens.</h4>
+        <Box mt={'20px'}>
+          <Text>
+            X imagens de Covid.
+          </Text>
+          <Text>
+            X imagens de Pulmão Saudável.
+          </Text>
+          <Text>
+            X imagens de Pneumonia.
+          </Text>
+          <Text>
+            X imagens de Tuberculose.
+          </Text>
+        </Box>
+      </div>
+      <div className='gerenciarIA-requests'>
+        <Button colorScheme='blue'>Solicitar novo treinamento</Button>
       </div>
     </div>
     
