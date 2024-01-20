b6f58cc6aa22e10ee40c8babbdf6b9f37004a1d1
Eduardo Tavares <eduardo_tvn@hotmail.com>
2024-01-20 00:31:30 -0300

    Administracao adicionada

--- src/components/Clinica/GerenciarIA.js
+++ src/components/Clinica/GerenciarIA.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import "./GerenciarIA.css";
-import { useState } from 'react';
 import {
-  Select,
   Box,
   Container,
   Grid,
@@ -11,34 +9,15 @@ import {
 } from '@chakra-ui/react'
 
 export const GerenciarIA = () => {
-  const [selectedModel, setSelectedModel] = useState(null);
-  const models = [
-    { value: '1', label: 'Pneumonia - Crianças de até 5 anos' },
-    { value: '2', label: 'Pneumonia, Covid, Tuberculose - mapa de calor' },
-  ]
 
   return (
     <div className='gerenciarIA-container'>
       <div className='gerenciarIA-top'>
         <h2>Gerenciar IA</h2>
       </div>
-      <div className='gerenciarIA-model-select'>
-        <Box w='30%' padding='2rem 0'>
-            <Box w='100%' >
-              <Select
-                value={selectedModel}
-                onChange={setSelectedModel}
-                options={models}
-                isSearchable
-                placeholder="Selecione um modelo"
-                background={'white'}
-              />
-            </Box>
-        </Box>
-      </div>
       <div className='gerenciarIA-model-details'>
         <Container background={'blue.600'} maxW={'container.lg'} h={40} borderRadius={'20px'} ml={'5'}>
-          <Box display={'flex'} alignItems={'center'} paddingTop={'8'}>
+          <Box display={'flex'} alignItems={'center'} justifyContent={'space-between'} paddingTop={'8'}>
             <Grid templateColumns={{ base: 'repeat(2, 1fr)', md: 'repeat(2, 1fr)', lg: 'repeat(2, 1fr)' }} gap={4}>
               <Container background={'white'} borderRadius={'10px'} mb={{ base: 2, md: 0 }} h={10} display={'flex'} alignItems={'center'}>
                 Precisão: "Valor placeholder"
@@ -53,6 +32,7 @@ export const GerenciarIA = () => {
                 Recall: "Valor placeholder"
               </Container>
             </Grid>
+            <Button colorScheme='blue'>Detalhes</Button>
           </Box>
         </Container>
       </div>
@@ -75,6 +55,7 @@ export const GerenciarIA = () => {
       </div>
       <div className='gerenciarIA-requests'>
         <Button colorScheme='blue'>Solicitar novo treinamento</Button>
+        <Button colorScheme='blue'>Reportar erro</Button>
       </div>
     </div>
     
---
+++ src/pages/Administrador/Administracao.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Administracao.css";
+import logo from '../../assets/noto_lungs.png'
+
+export const Administracao = () => {
+    return (
+        <div>
+            <div className="administracao-header">
+                <img src={logo}></img>
+                <h2>ADMINISTRAÇÃO</h2>
+            </div>
+            <div className="administracao-controls">
+                
+            </div>
+        </div>
+    )}
