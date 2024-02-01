f2997db15cb67e214e60679daa594fbebd833bf4
José Victor <56090571+ze-123@users.noreply.github.com>
2024-02-01 20:09:21 -0300

    update: redesign da página de genrenciar IA.
    Conexão de parte da página com o banco de dados.

--- src/components/Clinica/GerenciarIA.js
+++ src/components/Clinica/GerenciarIA.js
@@ -1,63 +1,112 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./GerenciarIA.css";
 import {
-  Box,
-  Container,
-  Grid,
-  Text,
   Button,
+  Stack,
+  TableContainer,
+  Table,
+  Tr,
+  Td,
+  Th,
+  Thead,
+  TableCaption,
+  Tbody,
+  Flex,
+  Icon,
 } from '@chakra-ui/react'
+import { CardModelo } from '../Cards/CardModelo';
+import { api } from '../../services/api';
+import { useSelector } from 'react-redux';
+import { RepeatIcon } from '@chakra-ui/icons';;
 
 export const GerenciarIA = () => {
+
+  const { data: user } = useSelector((state) => state.tokens);
+  const [classes, setClasses] = useState([])
+  const [numCasos, setNumCasos] = useState([]) 
+  const [modelos, setModelos] = useState([]) 
+  const [totalImagens, setTotalImagens] = useState(0)
+
+
+  const loadImagensTreinamento = async () => {
+    await api.post(`/diagnostico/imagens/treinamento`, {'clinica_id': user.data.id}).then(({ data }) => {
+      setClasses(data.classes)
+      setNumCasos(data.data)
+      setTotalImagens(data.data.at(-1))
+    }).catch( () => {
+      
+    })
+  }
+
+  const loadModelosClinca = async () => {
+    await api.get(`/modelo`, { cnpj: user.data.cnpj }).then( ({ data }) => {
+      console.log(data)
+      setModelos(data)
+    })
+  }
+
+
+  useEffect(() => {
+    loadImagensTreinamento().then(() => {})
+    loadModelosClinca().then(() => {})
+  }, [])
 
   return (
     <div className='gerenciarIA-container'>
       <div className='gerenciarIA-top'>
-        <h2>Gerenciar IA</h2>
+        <h2>Banco de imagens</h2>
       </div>
+
+      <div className='gerenciarIA-image-bank' >
+        <h4>Existem {totalImagens} imagens disponíveis para treinamento.</h4>
+        <Flex bg={'white'} shadow={'sm'} gap={4} padding={'10px'} borderRadius={'10px'}  width={'100%'} flexDirection={'row'} alignContent={'center'} justifyContent={'center'}>
+          <Flex w='50%'>
+            <TableContainer padding={'10px'}>
+              <Table variant='simple'>
+                <TableCaption>Imagens disponíveis para treinamento por diagnóstico.</TableCaption>
+                <Thead>
+                  <Tr>
+                    <Th>Diagnóstico</Th>
+                    <Th>Número de imagens</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  <Tr>
+                    <Td>{classes[0]}</Td>
+                    <Td fontWeight={'500'} color={'green.600'} >{numCasos[0]}</Td>
+                  </Tr>
+                  <Tr>
+                    <Td>{classes[1]}</Td>
+                    <Td fontWeight={'500'} color={'green.600'} >{numCasos[1]}</Td>
+                  </Tr>
+                  <Tr>
+                    <Td>{classes[2]}</Td>
+                    <Td fontWeight={'500'} color={'green.600'} >{numCasos[2]}</Td>
+                  </Tr>
+                  <Tr>
+                    <Td>{classes[3]}</Td>
+                    <Td fontWeight={'500'} color={'green.600'} >{numCasos[3]}</Td>
+                  </Tr>
+                </Tbody>
+              </Table>
+            </TableContainer>
+          </Flex>
+          <Flex w="30%" alignContent={'center'} textAlign={'center'} justifyContent={'center'} alignItems={'center'} flexWrap={'wrap'}>
+              <h3>Solicitar novo treinamento</h3>
+              <Button colorScheme='blue' w={'md'}><Icon as={RepeatIcon} /></Button>
+          </Flex>
+        </Flex>
+      </div>
+
+      <div className='gerenciarIA-top'>
+        <h2>Modelos</h2>
+      </div>
+
       <div className='gerenciarIA-model-details'>
-        <Container background={'blue.600'} maxW={'container.lg'} h={40} borderRadius={'20px'} ml={'5'}>
-          <Box display={'flex'} alignItems={'center'} justifyContent={'space-between'} paddingTop={'8'}>
-            <Grid templateColumns={{ base: 'repeat(2, 1fr)', md: 'repeat(2, 1fr)', lg: 'repeat(2, 1fr)' }} gap={4}>
-              <Container background={'white'} borderRadius={'10px'} mb={{ base: 2, md: 0 }} h={10} display={'flex'} alignItems={'center'}>
-                Precisão: "Valor placeholder"
-              </Container>
-              <Container background={'white'} borderRadius={'10px'} mb={{ base: 2, md: 0 }} h={10} display={'flex'} alignItems={'center'}>
-                Acurácia: "Valor placeholder"
-              </Container>
-              <Container background={'white'} borderRadius={'10px'} mb={{ base: 2, md: 0 }} h={10} display={'flex'} alignItems={'center'}>
-                F1-Score: "Valor placeholder"
-              </Container>
-              <Container background={'white'} borderRadius={'10px'} mb={{ base: 2, md: 0 }} h={10} display={'flex'} alignItems={'center'}>
-                Recall: "Valor placeholder"
-              </Container>
-            </Grid>
-            <Button colorScheme='blue'>Detalhes</Button>
-          </Box>
-        </Container>
-      </div>
-      <div className='gerenciarIA-image-bank'>
-        <h4>Existem X novas imagens no banco de imagens.</h4>
-        <Box mt={'20px'}>
-          <Text>
-            X imagens de Covid.
-          </Text>
-          <Text>
-            X imagens de Pulmão Saudável.
-          </Text>
-          <Text>
-            X imagens de Pneumonia.
-          </Text>
-          <Text>
-            X imagens de Tuberculose.
-          </Text>
-        </Box>
-      </div>
-      <div className='gerenciarIA-requests'>
-        <Button colorScheme='blue'>Solicitar novo treinamento</Button>
-        <Button colorScheme='blue'>Reportar erro</Button>
+        <Stack spacing={4} w={'90%'}>
+          <CardModelo modelo={{acuracia: '97.3', f1score:'98.4', nome: 'Modelo 001 - Pneumonia, Tuberculose, COVID-19'}} />
+        </Stack>
       </div>
     </div>
-    
   )
 }
