172a5e4edc4743db5355a19bcfb7a2a17b8dff96
Diego Vieira <dvsdiegovieira@gmail.com>
2024-02-02 07:33:28 -0300

    remove: comentarios e console.log

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const token = localStorage.getItem("@token")
-console.log("------------------")
+
 const headersAuth = {
   Authorization: `${token}`
 };
@@ -16,11 +16,11 @@ export const api = axios.create({
 
 const handleAuthenticationError = (error:any) => {
   if (error.response?.status === 401) {
-    // Redirecionar para a página de login ou fazer outras ações de autenticação
+
     window.location.href = '/login';
   }
   return Promise.reject(error);
 };
 
-// Adicione um interceptor para tratar erros de resposta
+
 api.interceptors.response.use((response) => response, handleAuthenticationError);
