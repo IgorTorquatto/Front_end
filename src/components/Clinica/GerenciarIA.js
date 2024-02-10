105075bfa120bd9786936863a7a6dd3e4c59d4bd
Diego Vieira <dvsdiegovieira@gmail.com>
2024-02-10 16:32:59 -0300

    feat: disable os finalizados

--- src/components/Clinica/GerenciarIA.js
+++ src/components/Clinica/GerenciarIA.js
@@ -22,7 +22,7 @@ import {
   ModalBody,
   ModalCloseButton,
   Text,
-  
+
 } from '@chakra-ui/react'
 import { CardModelo } from '../Cards/CardModelo';
 import { api } from '../../services/api';
@@ -53,12 +53,12 @@ export const GerenciarIA = () => {
   const loadImagensTreinamento = async () => {
 
     setIsLoadingTable(true)
-    await api.post(`/diagnostico/imagens/treinamento`, {'clinica_id': user.data.id}).then(({ data }) => {
+    await api.post(`/diagnostico/imagens/treinamento`, { 'clinica_id': user.data.id }).then(({ data }) => {
       setClasses(data.classes)
       setNumCasos(data.data)
       setTotalImagens(data.data.at(-1))
       setIsLoadingTable(false)
-    }).catch( () => {
+    }).catch(() => {
     })
   }
 
@@ -76,21 +76,21 @@ export const GerenciarIA = () => {
     }).catch(() => {
     })
   }
-    
+
 
   const loadModelosClinca = async () => {
     await api.get(`/modelo`).then(({ data }) => {
       setModelos(data.data)
-    }).catch(()=>{
-      
+    }).catch(() => {
+
     })
   }
 
   const loadRequisicoes = async () => {
     await api.get(`/requisicao?id_clinica=${user.data.id}`).then(({ data }) => {
       setRequisicoes(data.data)
-    }).catch(()=>{
-      
+    }).catch(() => {
+
     })
   }
 
@@ -105,7 +105,7 @@ export const GerenciarIA = () => {
       loadImagensTreinamento().then(() => { })
       updateDiagnosticos()
       loadRequisicoes()
-    }).catch(()=>{
+    }).catch(() => {
 
     })
   }
@@ -126,7 +126,7 @@ export const GerenciarIA = () => {
     loadModelosClinca().then(() => { })
     loadRequisicoes()
     loadDiagnosticos()
-    loadExisteSolicitacao().then(() => {})
+    loadExisteSolicitacao().then(() => { })
   }, [])
 
   // Função para verificar se a diferença entre duas datas é menor que 1 semana
@@ -147,20 +147,20 @@ export const GerenciarIA = () => {
       quantidade_imagens: totalImagens,
       id_clinica: user.data.id,
       data_hora: new Date(),
-    } 
+    }
     const data_email = {
       nome: user.data.nome,
       cnpj: user.data.cnpj,
       total_imagens: totalImagens,
       doenca: classes
     }
-    
-    await api.post('/requisicao', data_requisicao).then(()=>{
+
+    await api.post('/requisicao', data_requisicao).then(() => {
       loadImagensTreinamento().then(() => { })
       updateDiagnosticos()
       loadRequisicoes()
-    }).catch( (e) => { console.log(e)} )
-    await api.post('/email/requisicao', data_email).catch( (e) => { console.log(e)} )
+    }).catch((e) => { console.log(e) })
+    await api.post('/email/requisicao', data_email).catch((e) => { console.log(e) })
   }
 
   function handleSocitacaoTreinamento() {
@@ -176,15 +176,15 @@ export const GerenciarIA = () => {
         success: { title: 'Solitação enviada', description: 'Atualizações no modelo serão realizadas nas próximas semanas', duration: 6000 },
         error: { title: 'Falha', description: 'Solicitação não enviada', duration: 6000 },
         loading: { title: `Solicitando treinamento`, description: 'Por favor espere' },
-    })
+      })
   }
+  console.log(requisicoes)
 
   const disableRequisicao = () => {
-    if(requisicoes.length>0){
-      return diferencaMenorQueUmaSemana(requisicoes[0].data_hora,new Date()) || diagnosticos.length < 1
-    }
-    return true
-}
+    const emProgresso = requisicoes.some(item => item.status !== "Finalizado")
+    console.log(emProgresso)
+    return emProgresso || diagnosticos.length < 1
+  }
 
   return (
     <div className='gerenciarIA-container'>
@@ -193,87 +193,87 @@ export const GerenciarIA = () => {
       </div>
 
       <div className='gerenciarIA-image-bank' >
-        <Flex bg={'white'} shadow={'sm'} gap={4} padding={'10px'} borderRadius={'10px'}  width={'100%'} flexDirection={'row'} flexWrap={'wrap'} >
+        <Flex bg={'white'} shadow={'sm'} gap={4} padding={'10px'} borderRadius={'10px'} width={'100%'} flexDirection={'row'} flexWrap={'wrap'} >
           <Flex w={'100%'} alignContent={'center'} justifyContent={'center'}>
             <h4>Existem {totalImagens} imagens disponíveis para treinamento</h4>
           </Flex>
           <Flex w={'100%'} alignContent={'center'} justifyContent={'space-around'}>
             <Flex w='50%' justifyContent={'center'}>
-              { isLoadingTable ? <Spinner thickness='4px' size='lg'/> :
-              <TableContainer padding={'10px'}>
-                <Table variant='simple'>
-                  <Thead>
-                    <Tr>
-                      <Th>Diagnóstico</Th>
-                      <Th>Número de imagens</Th>
-                    </Tr>
-                  </Thead>
-                  <Tbody>
-                    <Tr>
-                      <Td>{classes[0]}</Td>
-                      <Td textAlign={'center'} fontWeight={'500'} color={'green.600'} > {numCasos[0]} </Td>
-                    </Tr>
-                    <Tr>
-                      <Td>{classes[1]}</Td>
-                      <Td textAlign={'center'} fontWeight={'500'} color={'green.600'} > {numCasos[1]} </Td>
-                    </Tr>
-                    <Tr>
-                      <Td>{classes[2]}</Td>
-                      <Td textAlign={'center'} fontWeight={'500'} color={'green.600'} > {numCasos[2]} </Td>
-                    </Tr>
-                    <Tr>
-                      <Td>{classes[3]}</Td>
-                      <Td textAlign={'center'} fontWeight={'500'} color={'green.600'} > {numCasos[3]} </Td>
-                    </Tr>
-                  </Tbody>
-                </Table>
-              </TableContainer>
+              {isLoadingTable ? <Spinner thickness='4px' size='lg' /> :
+                <TableContainer padding={'10px'}>
+                  <Table variant='simple'>
+                    <Thead>
+                      <Tr>
+                        <Th>Diagnóstico</Th>
+                        <Th>Número de imagens</Th>
+                      </Tr>
+                    </Thead>
+                    <Tbody>
+                      <Tr>
+                        <Td>{classes[0]}</Td>
+                        <Td textAlign={'center'} fontWeight={'500'} color={'green.600'} > {numCasos[0]} </Td>
+                      </Tr>
+                      <Tr>
+                        <Td>{classes[1]}</Td>
+                        <Td textAlign={'center'} fontWeight={'500'} color={'green.600'} > {numCasos[1]} </Td>
+                      </Tr>
+                      <Tr>
+                        <Td>{classes[2]}</Td>
+                        <Td textAlign={'center'} fontWeight={'500'} color={'green.600'} > {numCasos[2]} </Td>
+                      </Tr>
+                      <Tr>
+                        <Td>{classes[3]}</Td>
+                        <Td textAlign={'center'} fontWeight={'500'} color={'green.600'} > {numCasos[3]} </Td>
+                      </Tr>
+                    </Tbody>
+                  </Table>
+                </TableContainer>
               }
             </Flex>
-            <Flex w="30%" alignContent={'center'} textAlign={'center'} justifyContent={'center'}  flexWrap={'wrap'}>
-                <h3>Solicitar novo treinamento</h3>
-                <Button colorScheme='blue' w={'md'} isLoading={loadingButton} isDisabled={solicitado} onClick={onOpen}><Icon as={RepeatIcon} /></Button>
-                { solicitacao &&
+            <Flex w="30%" alignContent={'center'} textAlign={'center'} justifyContent={'center'} flexWrap={'wrap'}>
+              <h3>Solicitar novo treinamento</h3>
+              <Button colorScheme='blue' w={'md'} isLoading={loadingButton} isDisabled={disableRequisicao()} onClick={onOpen}><Icon as={RepeatIcon} /></Button>
+              {disableRequisicao() &&
                 <Text fontWeight={'500'} title={solicitacao.data_hora} mt={4}>*Solicitação ainda não atendida</Text>
-                }
+              }
             </Flex>
           </Flex>
         </Flex>
       </div>
-{requisicoes.length > 0 &&
-<>
-<div className='gerenciarIA-top'>
-        <h2>Requisições</h2>
-      </div>
+      {requisicoes.length > 0 &&
+        <>
+          <div className='gerenciarIA-top'>
+            <h2>Requisições</h2>
+          </div>
 
-      <div className='gerenciarIA-model-details'>
-        <Stack spacing={4} w={'90%'}>
-          
-          {requisicoes.map(item => (
-            <CardRequisicao requisicao={item} />
-          ))}
-        </Stack>
-      </div>
-</>
-     }
-{
-  modelos.length > 0 && <>
-  <div className='gerenciarIA-top'>
-        <h2>Modelos</h2>
-      </div>
+          <div className='gerenciarIA-model-details'>
+            <Stack spacing={4} w={'90%'}>
 
-      <div className='gerenciarIA-model-details'>
-        <Stack spacing={4} w={'90%'}>
-          {
-            modelos.map((modelo, index) => {
-              return <CardModelo modelo={modelo} />
-            })
-          }
-        </Stack>
-      </div>
-  </>
-}
-      
+              {requisicoes.map(item => (
+                <CardRequisicao requisicao={item} />
+              ))}
+            </Stack>
+          </div>
+        </>
+      }
+      {
+        modelos.length > 0 && <>
+          <div className='gerenciarIA-top'>
+            <h2>Modelos</h2>
+          </div>
+
+          <div className='gerenciarIA-model-details'>
+            <Stack spacing={4} w={'90%'}>
+              {
+                modelos.map((modelo, index) => {
+                  return <CardModelo modelo={modelo} />
+                })
+              }
+            </Stack>
+          </div>
+        </>
+      }
+
 
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
@@ -281,7 +281,7 @@ export const GerenciarIA = () => {
           <ModalHeader>Solicitar novo treinamento</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            Ao clicar em "Solicitar", será feita uma requisição para que se use as novas imagens no treinamento de um modelo.  
+            Ao clicar em "Solicitar", será feita uma requisição para que se use as novas imagens no treinamento de um modelo.
           </ModalBody>
 
           <ModalFooter>
